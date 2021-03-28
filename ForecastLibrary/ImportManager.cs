a1aa7c94ca05c07776bbd6cbf9f3afc7dea1646b
Mikuláš Hoblík <mikuhoblik@gmail.com>
2021-03-28 09:59:32 +0200

    Update

--- ForecastLibrary/ImportManager.cs
+++ ForecastLibrary/ImportManager.cs
@@ -66,7 +66,7 @@ namespace ForecastLibrary
                 ImportSalesData();
 
                 return true;
-            }
+        }
             catch (NullReferenceException)
             {
                 return false;
